3ce1b6613e275a752bbe79af8cd57c38cd41338d
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-13 17:28:45 +1300

    Sort by category(1)

---
+++ SportsStore/Controls/CategoryList.ascx.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsStore.Controls
+{
+    public partial class CategoryList : System.Web.UI.UserControl
+    {
+        Repository repo = new Repository();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void CreateHomeLinkHtml()
+        {
+            var query = repo.Products;
+        }
+    }
+}
