32b873ac2a488effca8ef5ffacb19cd5ac83e791
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-21 23:39:40 +0800

    building administration page

---
+++ SportsStore/Pages/Admin/Admin.Master.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsStore.Pages.Admin
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string OrdersUrl
+        {
+            get
+            {
+                return generateURL("admin_orders");
+            }
+        }
+        public string ProductsUrl
+        {
+            get
+            {
+                return generateURL("admin_products");
+            }
+        }
+        private string generateURL(string routeName)
+        {
+            return RouteTable.Routes.GetVirtualPath(null, routeName, null).VirtualPath;
+        }
+    }
+}
---
+++ SportsStore/Pages/Admin/Orders.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Orders.aspx.cs" Inherits="SportsStore.Pages.Admin.Orders" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="outerContainer">
+        <table class="orderTable">
+            <tr><th>Name</th><th>City</th><th>Items</th><th>Total</th><th></th></tr>
+        </table>
+    </div>
+</asp:Content>
