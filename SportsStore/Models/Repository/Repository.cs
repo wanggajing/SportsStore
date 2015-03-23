9c54f9872cdb6a74c16877a002a730ac32176963
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-23 17:07:37 +1300

    Allow administrator to manage the products(CRUD)

--- SportsStore/Models/Repository/Repository.cs
+++ SportsStore/Models/Repository/Repository.cs
@@ -53,5 +53,37 @@ namespace SportsStore.Models.Repository
             }
             context.SaveChanges();
         }
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                product = context.Products.Add(product);
+            }
+            else
+            {
+                Product dbProduct = context.Products.Find(product.ProductID);
+                if (dbProduct != null)
+                {
+                    dbProduct.Name = product.Name;
+                    dbProduct.Description = product.Description;
+                    dbProduct.Price = product.Price;
+                    dbProduct.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
+        public void DeleteProduct(Product product)
+        {
+            IEnumerable<Order> orders = context.Orders
+                .Include(o => o.OrderLines.Select(ol => ol.Product))
+                .Where(o => o.OrderLines.Count(ol => ol.Product
+                .ProductID == product.ProductID) > 0).ToArray();
+            foreach (Order order in orders)
+            {
+                context.Orders.Remove(order);
+            }
+            context.Products.Remove(product);
+            context.SaveChanges();
+        }
     }
 }
